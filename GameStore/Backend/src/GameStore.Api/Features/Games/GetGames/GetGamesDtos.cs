ae18fe9ca03afc7c2c79c460572576f9f31a7c51
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2026-09-04 10:26:25 +1000

    Completed up to pagination

--- GameStore/Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesDtos.cs
+++ GameStore/Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesDtos.cs
@@ -1,5 +1,14 @@
 namespace GameStore.Api.Features.Games.GetGames;
 
+public record GetGamesDto(
+    string? Name = null,
+    int PageNumber = 1,
+    int PageSize = 5);
+
+public record GamesPageDto(
+    int TotalPages,
+    IReadOnlyList<GameSummaryDto> Games);
+
 public record GameSummaryDto(
     Guid Id,
     string Name,
--- GameStore/Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ GameStore/Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -1,4 +1,6 @@
 using GameStore.Api.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Games.GetGames;
@@ -8,21 +10,50 @@ public static class GetGamesEndpoint
     // GET /games
     public static void MapGetGames(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/", async (GameStoreContext dbContext) =>
-            TypedResults.Ok<IReadOnlyList<GameSummaryDto>>(
-                await dbContext
-                        .Games
-                        .Include(x => x.Genre)                  // Eager Load Navigational Property
-                        .AsNoTracking()                         // No Tracking - Read-Only Query
-                        .Select(x => new GameSummaryDto(
-                                            x.Id,
-                                            x.Name,
-                                            x.Genre!.Name,
-                                            x.Price,
-                                            x.ReleaseDate))
-                        .ToListAsync()
+        app.MapGet("/",
+            async Task<Ok<GamesPageDto>> (
+            [AsParameters] GetGamesDto request,
+            GameStoreContext dbContext
+            ) =>
+        {
+            // build with where clause if name provided
+            var gamesQuery = dbContext.Games
+                                        .Where(x =>
+                                            string.IsNullOrWhiteSpace(request.Name) ||
+                                                EF.Functions.Like(
+                                                    x.Name.ToLower(),
+                                                    $"%{request.Name.ToLower()}%"));
 
-            )
+            // find total page count
+            var totalRecords = await gamesQuery.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalRecords / (float)request.PageSize);
+
+            // get paged records
+            var skipCount = (request.PageNumber - 1) * request.PageSize;
+            var paginatedQuery = gamesQuery
+                                    .OrderBy(x => x.Name)
+                                    .Include(x => x.Genre)                      // Eager Load Navigational Property                              
+                                    .AsNoTracking()                             // No Tracking - Read-Only Query      
+                                    .Skip(skipCount)
+                                    .Take(request.PageSize)
+                                    .Select(x => new GameSummaryDto(
+                                        x.Id,
+                                        x.Name,
+                                        x.Genre!.Name,
+                                        x.Price,
+                                        x.ReleaseDate
+                                    ));
+            var gamesOnPage = await paginatedQuery.ToListAsync();
+
+            return TypedResults.Ok(
+                new GamesPageDto(
+                    totalPages,
+                    gamesOnPage
+                    )
+            );
+        }
+
+
         );
     }
 }
